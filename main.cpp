07a6482135a28289b44c8d8fd5ac90bc08f49ce5
Rustenis <r.tolpeznikas@gmail.com>
2025-05-27 21:57:04 +0200

    Disk size limits

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ void printAbout() {
 void printUsage(const string &programName) {
     cout << "Usage: " << programName << " <command> [options]" << endl;
     cout << "----------------------------------------" << endl;
-    cout << "dmake <diskfile> [size_bytes] <- Create a new virtual disk file with optional size (default 10MB)" << endl;
+    cout << "dmake   <diskfile> [size_bytes] <- Create a new virtual disk file with optional size" << "\n" <<
+            "(default 10MB, min 4096 bytes, max 100MB)" << endl;
     cout << "dremove <diskfile> <- Remove the virtual disk file" << endl;
-    cout << "dput <diskfile> <localfile> <- Copy a local file to the virtual disk" << endl;
-    cout << "dget <diskfile> <filename> [dest] <- Copy a file from the virtual disk" << endl;
-    cout << "ddel <diskfile> <filename> <- Deletes a file from the virtual disk" << endl;
-    cout << "dls <diskfile> <- List files in the virtual disk" << endl;
-    cout << "dmap <diskfile> <- Show block occupation on the virtual disk" << endl;
+    cout << "dput    <diskfile> <localfile> <- Copy a local file to the virtual disk" << endl;
+    cout << "dget    <diskfile> <filename> [dest] <- Copy a file from the virtual disk" << endl;
+    cout << "ddel    <diskfile> <filename> <- Deletes a file from the virtual disk" << endl;
+    cout << "dls     <diskfile> <- List files in the virtual disk" << endl;
+    cout << "dmap    <diskfile> <- Show block occupation on the virtual disk" << endl;
     cout << "help <- Show this help message" << endl;
     cout << "about <- For more information about the program" << endl;
 }
@@ -42,9 +43,16 @@ int main(const int argc, char *argv[]) {
         uint32_t size = DEFAULT_DISK_SIZE;
         if (argc >= 4) {
             size = static_cast<uint32_t>(stoul(argv[3]));
+            // We should also check for size in here
+            // too small would just cause a crash
+            // However, too big can cause serious issues
+            // Limiting to 100 MB for now
+            if (size < 4096 || size > 100 * 1024 * 1024) {
+                cerr << "Error: Disk size must be between 4096 bytes and 100 MB." << endl;
+                return 1;
+            }
         }
         if (VirtualFileSystem vfs(diskName); !vfs.createDisk(size)) return 1;
-
     } else if (cmd == "dremove") {
         if (argc < 3) {
             printUsage(argv[0]);
@@ -54,7 +62,6 @@ int main(const int argc, char *argv[]) {
         const string diskName = argv[2];
         VirtualFileSystem vfs(diskName);
         vfs.removeDisk();
-
     } else if (cmd == "dput") {
         if (argc < 4) {
             printUsage(argv[0]);
@@ -66,7 +73,6 @@ int main(const int argc, char *argv[]) {
         VirtualFileSystem vfs(diskName);
         if (!vfs.loadDisk()) return 1;
         vfs.copyFromHost(hostFile);
-
     } else if (cmd == "dget") {
         if (argc < 4) {
             printUsage(argv[0]);
@@ -79,7 +85,6 @@ int main(const int argc, char *argv[]) {
         VirtualFileSystem vfs(diskName);
         if (!vfs.loadDisk()) return 1;
         vfs.copyToHost(fileName, dest);
-
     } else if (cmd == "ddel") {
         if (argc < 4) {
             printUsage(argv[0]);
@@ -91,7 +96,6 @@ int main(const int argc, char *argv[]) {
         VirtualFileSystem vfs(diskName);
         if (!vfs.loadDisk()) return 1;
         vfs.deleteFile(fileName);
-
     } else if (cmd == "dls") {
         if (argc < 3) {
             printUsage(argv[0]);
@@ -102,7 +106,6 @@ int main(const int argc, char *argv[]) {
         VirtualFileSystem vfs(diskName);
         if (!vfs.loadDisk()) return 1;
         vfs.listFiles();
-
     } else if (cmd == "dmap") {
         if (argc < 3) {
             printUsage(argv[0]);
@@ -113,7 +116,6 @@ int main(const int argc, char *argv[]) {
         VirtualFileSystem vfs(diskName);
         if (!vfs.loadDisk()) return 1;
         vfs.showMap();
-
     } else if (cmd == "help") {
         printUsage(argv[0]);
         return 0;
